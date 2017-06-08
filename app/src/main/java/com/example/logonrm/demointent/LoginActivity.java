042739f0eb4a653b1e150caa7affb135b26ed8b3
logonrm <vini.torresc@gmail.com>
2017-06-07 22:42:35 -0300

    BroadcastReceiver Exemplo

--- app/src/main/java/com/example/logonrm/demointent/LoginActivity.java
+++ app/src/main/java/com/example/logonrm/demointent/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.logonrm.demointent;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -21,12 +25,15 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        requestSmsPermission();
     }
 
     @OnClick(R.id.tvNovoUsuario)
     public void novoUsuarioClick(){
         Intent novoUsuario = new Intent(this,NovoUsuarioActivity.class);
         startActivityForResult(novoUsuario,1);
+
     }
 
     @Override
@@ -40,4 +47,16 @@ public class LoginActivity extends AppCompatActivity {
         }
 
     }
+
+    private void requestSmsPermission() {
+        String permission = Manifest.permission.RECEIVE_SMS;
+        int grant = ContextCompat.checkSelfPermission(this, permission);
+        if ( grant != PackageManager.PERMISSION_GRANTED) {
+            String[] permission_list = new String[1];
+            permission_list[0] = permission;
+            ActivityCompat.requestPermissions(this, permission_list, 1);
+        }
+    }
+
+
 }
